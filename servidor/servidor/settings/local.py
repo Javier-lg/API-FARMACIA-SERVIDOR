106e7db8dafa2479ab61964a25e45e71f1b518ac
javier-lg <javiersalinasdev@gmail.com>
2024-12-14 00:30:00 -0300

    app farmacia | inspectdb insumos

--- servidor/servidor/settings/local.py
+++ servidor/servidor/settings/local.py
@@ -7,9 +7,16 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Usamos SQLite en lugar de PostgreSQL
         'NAME': BASE_DIR / 'db.sqlite3',  # Ruta a la base de datos SQLite
+    },
+    'SAFESA_TM':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('SAFESA_TM_DBNAME'),
+        'USER': os.getenv('SAFESA_TM_USER'),
+        'PASSWORD': os.getenv('SAFESA_TM_PASSWORD'),
+        'HOST': os.getenv('SAFESA_TM_HOST'),
+        'PORT': os.getenv('SAFESA_TM_PORT')
     }
 }
-
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
