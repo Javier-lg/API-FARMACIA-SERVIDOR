d97ae16d012aeafe6db215ce52aee1d3f5f4d154
javier-lg <javiersalinasdev@gmail.com>
2024-12-14 19:37:35 -0300

    command configurado | apps conexiones

--- servidor/apps/farmacia/management/commands/listen_for_table_changes.py
+++ servidor/apps/farmacia/management/commands/listen_for_table_changes.py
@@ -4,15 +4,14 @@ import json
 import psycopg2
 
 class Command(BaseCommand):
-    help = "Escucha cambios en la tabla 'tm_insumosmedicos'."
+    help = "Escucha cambios en la tabla 'tm_insumosmedicos' y genera le mensaje a enviar."
 
     def handle(self, *args, **kwargs):
-        db_alias = "SAFESA_TM"  # Cambia esto si deseas usar otra base de datos configurada
+        db_alias = "SAFESA_TM"  # base de datos registrada en settings/local.py
         connection = connections[db_alias]
 
-        # Verificar que estamos usando PostgreSQL
         if connection.vendor != 'postgresql':
-            self.stderr.write(self.style.ERROR("Este comando solo funciona con bases de datos PostgreSQL."))
+            self.stderr.write(self.style.ERROR("Esto solo funciona con PostgreSQL."))
             return
 
         self.stdout.write(self.style.SUCCESS(f"Conectando a la base de datos '{connection.settings_dict['NAME']}'..."))
@@ -34,6 +33,13 @@ class Command(BaseCommand):
                         self.stdout.write(f"Datos nuevos: {payload['data']}")
                         if 'old_data' in payload:
                             self.stdout.write(f"Datos antiguos: {payload['old_data']}")
+
+                        if payload['operation'] == 'INSERT':
+                            self.notificar_servidores(payload['operation'], payload['data'])
+                        elif payload['operation'] == 'UPDATE':
+                            self.notificar_servidores(payload['operation'], payload['data'], payload['old_data'])
+                        elif payload['operation'] == 'DELETE':
+                            self.notificar_servidores(payload['operation'], payload['data'])
         except OperationalError as e:
             self.stderr.write(self.style.ERROR(f"Error de base de datos: {e}"))
         except KeyboardInterrupt:
@@ -48,3 +54,22 @@ class Command(BaseCommand):
                     self.stdout.write(self.style.SUCCESS("Conexión cerrada correctamente."))
                 except Exception as e:
                     self.stderr.write(self.style.ERROR(f"Error al cerrar la conexión: {e}"))
+
+    def notificar_servidores(self, operacion, datos, antiguos_datos=None):
+        if operacion == "INSERT":
+            payload = {
+                "operation": operacion,
+                "data": datos
+            }
+        elif operacion == "UPDATE":
+            payload = {
+                "operation": operacion,
+                "data": datos,
+                "old_data": antiguos_datos
+            }
+        elif operacion == "DELETE":
+            payload = {
+                "operation": operacion,
+                "data": datos
+            }
+        self.stderr.write(self.style.SUCCESS(payload))
